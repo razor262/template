43dddae3855ef16b6d46f2638ac2997f019eaeb7
razor262 <24.work@bk.ru>
2024-04-07 16:33:10 +0300

    two: second commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
 console.log('work'); 
+
+/* ---------------------------- */
+/* burger menu */ 
+/* ---------------------------- */
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let body = document.querySelector('.body');
+
+menuBtn.addEventListener('click', function () {
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+	body.classList.toggle('active-body');
+}); 
+
+/* ---------------------------- */
+/* spoiler */
+/* ---------------------------- */
+
+"use strict"; 
